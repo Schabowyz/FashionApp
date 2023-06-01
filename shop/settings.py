3c6264e5500400cadb1244b2821fe7512cfda0c1
Damian <ddamian.19962@gmail.com>
2023-06-01 23:33:32 +0200

    Cleared the situation with staticfiles

--- shop/settings.py
+++ shop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 from .keys import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
