a492998daf8c03ba583ea8b0ba45dcab25e50185
Damian <ddamian.19962@gmail.com>
2023-06-24 21:19:58 +0200

    fixes continued

--- shop/settings.py
+++ shop/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
@@ -204,7 +204,7 @@ SOCIALACCOUNT_ADAPTER = "user.models.CustomSocialAccountAdapter"
 
 STRIPE_SECRET_KEY = stripe_secret_key
 PRODUCT_PRICE = 50
-REDIRECT_DOMAIN = "http://127.0.0.1:8000/user/order"
+REDIRECT_DOMAIN = "https://fashionappshop.fly.dev/user/order"
 
 
 # AWS MEDIA STORAGE WITH S3
